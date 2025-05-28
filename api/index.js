a4c23eda5f4eb9449b933b77605c237c726776a7
frankamale <frankamale5@gmail.com>
2025-05-28 12:48:14 +0300

    Added crud for category

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRouter from "../routes/auth.routes.js";
 import helmet from "helmet";
 import productRouter from "../routes/product.routes.js";
 import brandRouter from "../routes/brand.routes.js";
+import categoryRouter from "../routes/category.route.js";
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get("/status", (_req, res) => {
 app.use("/api", authRouter);
 app.use("/api", productRouter);
 app.use("/api", brandRouter);
+app.use("/api", categoryRouter);
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`App listening on port: ${PORT}`));
 });
