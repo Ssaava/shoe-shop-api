cf450e47f893c7c359f2caf7268feeba096c973c
Ssaava <ssavaemma4@gmail.com>
2025-06-21 22:14:05 +0300

    updated cart controller functionality

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,21 +1,18 @@
 import { checkAuthentication } from "../middlewares/auth.middleware.js";
 
-
 import { Router } from "express";
 import {
   addToCart,
   getUserCart,
-  updateCartItem,
   removeCartItem,
-  clearCart
+  clearCart,
 } from "../controllers/cart.controller.js";
 
 const cartRouter = Router();
 
 cartRouter.post("/cart/add", checkAuthentication, addToCart);
 cartRouter.get("/cart", checkAuthentication, getUserCart);
-cartRouter.put("/cart/update", checkAuthentication, updateCartItem);
-cartRouter.delete("/cart/remove/:productId", checkAuthentication, removeCartItem);
-cartRouter.delete("/cart/clear", checkAuthentication, clearCart);
+cartRouter.patch("/cart/remove-product", checkAuthentication, removeCartItem);
+cartRouter.delete("/cart/clear-cart", checkAuthentication, clearCart);
 
 export default cartRouter;
