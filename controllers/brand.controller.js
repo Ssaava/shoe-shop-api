8970331389f44499d26932af3297a9071705b913
Ssaava <ssavaemma4@gmail.com>
2025-06-22 10:50:26 +0300

    updated brand controller and routes

--- controllers/brand.controller.js
+++ controllers/brand.controller.js
@@ -1,57 +1,143 @@
+import mongoose from "mongoose";
 import Brand from "../models/brand.model.js";
+import Product from "../models/product.model.js";
 
-export const registerBrand = async (req, res) => {
+export const createBrand = async (req, res) => {
+  const { name } = req.body;
   try {
-    const brand = new Brand(req.body);
+    if (!name) {
+      return res.status(400).json({
+        success: false,
+        message: "Brand name is required",
+      });
+    }
+    const existingBrand = await Brand.findOne({ name });
+    if (existingBrand) {
+      return res.status(400).json({
+        success: false,
+        message: "Brand name already exists",
+      });
+    }
+    const brand = new Brand({ name });
     await brand.save();
-    res.status(201).json({ message: "Brand registered successfully", brand });
+    return res
+      .status(201)
+      .json({ success: true, message: "Brand registered successfully", brand });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log("Brand Creation Error:  ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create brand",
+      error: error.message,
+    });
   }
 };
 
-export const getBrands = async (_req, res) => {
+export const fetchAllBrands = async (_req, res) => {
   try {
     const brands = await Brand.find();
-    res.status(200).json({ message: "Brands fetched", brands });
+    return res.status(200).json({ success: true, brands });
   } catch (error) {
-    res.status(404).json({ error: error.message });
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch brands",
+      error: error.message,
+    });
   }
 };
 
-export const getBrand = async (req, res) => {
+export const getBrandById = async (req, res) => {
+  const { brandId } = req.params;
   try {
-    const brand = await Brand.findById(req.params.id);
-    if (!brand) return res.status(404).json({ message: "Brand not found" });
-    return res.status(200).json({ message: "Brand Fetched", brand });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export const deleteBrand = async (req, res) => {
-  try {
-    const brand = await Brand.findByIdAndDelete(req.params.id);
+    const brand = await Brand.findById(brandId);
     if (!brand) {
-      return res.status(404).json({ message: "Brand not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Brand not found" });
     }
-    return res.status(200).json({ message: "Brand deleted successfully" });
+    return res.status(200).json({ success: true, brand });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    return res.status(500).json({
+      success: false,
+      message: "Failed to Fetch Brand",
+      error: error.message,
+    });
   }
 };
 
 export const updateBrand = async (req, res) => {
+  const { brandId } = req.params;
+  const { name } = req.body;
   try {
-    const brand = await Brand.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!brand) {
-      return res.status(404).json({ message: "Brand not found" });
+    if (!name) {
+      return res.status(400).json({
+        success: false,
+        message: "Brand name is required",
+      });
     }
-    return res.status(200).json({ message: "Brand updated", brand });
+    const existingBrand = await Brand.findOne({ name });
+
+    if (existingBrand && existingBrand.name === name) {
+      return res.status(400).json({
+        success: false,
+        message: "Same Brand name cannot update Brand",
+      });
+    }
+    const brand = await Brand.findByIdAndUpdate(
+      brandId,
+      { $set: { name } },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if (!brand) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Brand not found" });
+    }
+    return res.status(200).json({ success: true, brand });
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({
+      success: false,
+      message: "Failed to update Brand",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteBrand = async (req, res) => {
+  const { brandId } = req.params;
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const brand = await Brand.findById(brandId);
+    if (!brand) {
+      await session.abortTransaction();
+      return res
+        .status(404)
+        .json({ success: false, message: "Brand not found" });
+    }
+    await Product.updateMany(
+      { brand: brandId },
+      { $unset: { brand: "" } },
+      { session }
+    );
+
+    await Brand.findByIdAndDelete(brandId).session(session);
+    await session.commitTransaction();
+    return res
+      .status(200)
+      .json({ success: true, message: "Brand deleted successfully" });
+  } catch (error) {
+    console.log("Brand Delete Error: ", error);
+    await session.abortTransaction();
+    return res.status(500).json({
+      success: false,
+      message: "Failed to delete brand",
+      error: error.message,
+    });
+  } finally {
+    session.endSession();
   }
 };
--- models/brand.model.js
+++ models/brand.model.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
-const brandSchema = new mongoose.Schema({
-  name: { type: String },
-});
+const brandSchema = new mongoose.Schema(
+  {
+    name: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Brand = mongoose.model("Brand", brandSchema);
 
-export default Brand ;
+export default Brand;
